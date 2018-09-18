e59dc9f233e6e85510bbb63ca25cb483de2ab932
Red Fox <sarahanne118@gmail.com>
2018-09-17 19:02:53 -0600

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
-//create targetNumber variable
+
 var targetNumber = 0;
 
-//create crystalNumber variable 
+
 var crystalNumber = [Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1)];
 
 var images = ["assets/images/crystal 1.jpg", "assets/images/crystal 2.jpg", "assets/images/crystal 3.jpg", "assets/images/crystal 4.jpg"];
 
 var crystalImage;
-//maybe make this a function we can call later
+
 for (var i = 0; i < crystalNumber.length; i++) {
     crystalImage = $("<img>");
     crystalImage.addClass("crystal-image");
@@ -25,11 +25,11 @@ var losses = 0;
 
 function resetGame() {
 
-    //resetting user's score
+   
     totalScore = 0
     $("#total-score").html(totalScore);
 
-    //reset target number
+    
     targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
     $("#target-number").html(targetNumber);
 
@@ -55,14 +55,14 @@ $(".crystal-image").on("click", function () {
     if (totalScore === targetNumber) {
         wins++;
         $("#wins").html(wins);
-        // $("#wins-losses").text("You won!");
+        $("#wins-losses").text("You won!");
         resetGame();
     }
 
     else if (totalScore >= targetNumber) {
         losses++;
         $("#losses").html(losses);
-        // $("#wins-losses").html("You lost!");
+        $("#wins-losses").html("You lost!");
         resetGame();
     }
 
